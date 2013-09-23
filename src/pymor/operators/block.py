94e0638f1f7fa70909801f79724553f6b5f16e2e
Felix Albrecht <mail@felixalbrecht.de>
2013-09-23 19:46:24 +0200

    [operators.block] added some dim_range != 1 functionality

--- src/pymor/operators/block.py
+++ src/pymor/operators/block.py
@@ -6,6 +6,7 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
+from scipy.sparse import coo_matrix, bmat
 
 from pymor.operators.interfaces import OperatorInterface
 from pymor.la.interfaces import VectorArrayInterface
@@ -16,9 +17,10 @@ from pymor.operators.basic import NumpyMatrixOperator, NumpyMatrixBasedOperator
 
 class BlockOperator(OperatorInterface):
 
-    def __init__(self, blocks):
+    def __init__(self, blocks, source_dims=None, range_dims=None):
         assert isinstance(blocks, list)
         if isinstance(blocks[0], OperatorInterface):
+            assert source_dims is None and range_dims is None
             assert all([isinstance(block, OperatorInterface) for block in blocks])
             if blocks[0].dim_range == 1:
                 assert all([block.dim_range == 1 for block in blocks])
@@ -33,7 +35,23 @@ class BlockOperator(OperatorInterface):
                 raise Exception('Not implemented yet!')
             self.dim_source = sum([block.dim_source for block in blocks])
         else:
-            raise Exception('Not implemented yet!')
+            assert source_dims is not None and range_dims is not None
+            assert isinstance(source_dims, list) and isinstance(range_dims, list)
+            self._range_dims = range_dims
+            self._source_dims = source_dims
+            self.dim_range = sum(self._range_dims)
+            self.dim_source = sum(self._source_dims)
+            self.num_source_blocks = len(self._source_dims)
+            self.num_range_blocks = len(self._range_dims)
+            assert len(blocks) == len(self._range_dims)
+            assert all([len(block_row) == len(self._source_dims) for block_row in blocks])
+            assert all([all([isinstance(blocks[ii][jj], OperatorInterface) if blocks[ii][jj] is not None else True for jj in np.arange(self.num_source_blocks)]) for ii in np.arange(self.num_range_blocks)])
+            assert all([all([blocks[ii][jj].dim_source == self._source_dims[jj] if blocks[ii][jj] is not None else True for jj in np.arange(self.num_source_blocks)]) for ii in np.arange(self.num_range_blocks)])
+            assert all([all([blocks[ii][jj].dim_range == self._range_dims[ii] if blocks[ii][jj] is not None else True for jj in np.arange(self.num_source_blocks)]) for ii in np.arange(self.num_range_blocks)])
+            self._blocks = blocks
+            self.linear = all([all([block.linear if block is not None else True for block in blocks_row]) for blocks_row in self._blocks])
+            self.invert_options = None
+            self.type_range = BlockVectorArray
         self.type_source = BlockVectorArray
         self.lock()
 
@@ -44,7 +62,14 @@ class BlockOperator(OperatorInterface):
         raise Exception('Not implemented yet!')
 
     def apply_inverse(self, U, ind=None, mu=None, options=None):
-        raise Exception('Not implemented yet!')
+        op = NumpyMatrixOperator(bmat([[coo_matrix(self._blocks[ii][jj].assemble(mu)._matrix) if self._blocks[ii][jj] is not None
+                                        else coo_matrix((self._range_dims[ii], self._source_dims[jj]))
+                                        for jj in np.arange(self.num_source_blocks)]
+                                       for ii in np.arange(self.num_range_blocks)]))
+        #import ipdb; ipdb.set_trace()
+        res = op.apply_inverse(U, ind, options)
+        assert len(res) == 1  # not implemented yet
+        return BlockVectorArray(res, self._source_dims)
 
     @staticmethod
     def lincomb(operators, coefficients=None, num_coefficients=None, coefficients_name=None, name=None):
@@ -67,10 +92,23 @@ class BlockOperator(OperatorInterface):
         elif isinstance(source_basis, list):
             if self.dim_range == 1:
                 assert len(source_basis) == self.num_source_blocks
-                assert all([source_basis[ss].dim == self._blocks[ss].dim_source for ss in np.arange(self.num_source_blocks)])
-                return BlockOperator([self._blocks[ss].projected(source_basis[ss], range_basis=None, product=None) for ss in np.arange(self.num_source_blocks)])
+                assert all([source_basis[jj].dim == self._blocks[jj].dim_source for jj in np.arange(self.num_source_blocks)])
+                return BlockOperator([self._blocks[jj].projected(source_basis[jj], range_basis=None, product=None) for jj in np.arange(self.num_source_blocks)])
             else:
-                raise Exception('Not implemented yet!')
+                assert len(source_basis) == self.num_source_blocks
+                source_rb = source_basis
+                assert all([source_rb[jj].dim == self._source_dims[jj] for jj in np.arange(self.num_source_blocks)])
+                range_rb = range_basis if range_basis is not None else source_basis
+                assert len(range_rb) == self.num_range_blocks
+                assert all([range_rb[ii].dim == self._range_dims[ii] for ii in np.arange(self.num_range_blocks)])
+                return BlockOperator([[self._blocks[ii][jj].projected(source_rb[jj], range_rb[ii]) if self._blocks[ii][jj] is not None
+                                       else None
+                                       for jj in np.arange(self.num_source_blocks)]
+                                      for ii in np.arange(self.num_range_blocks)],
+                                     [len(base) for base in source_rb],
+                                     [len(base) for base in range_rb])
+        else:
+            raise Exception('Invalid source_basis given!')
 
     def assemble(self, mu=None):
         assert all([isinstance(block, NumpyMatrixBasedOperator) for block in self._blocks])
@@ -78,3 +116,7 @@ class BlockOperator(OperatorInterface):
             return NumpyMatrixOperator(np.concatenate([block.assemble(mu).as_vector().data for block in self._blocks], axis=1))
         else:
             raise Exception('Not implemented yet!')
+
+    def as_vector(self, mu=None):
+        assert self.dim_range == 1
+        return NumpyVectorArray(np.concatenate([self._blocks[jj].assemble(mu)._matrix for jj in np.arange(self.num_source_blocks)], axis=1))
