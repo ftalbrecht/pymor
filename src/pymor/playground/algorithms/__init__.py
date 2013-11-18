c810dfef62adbb6ef91494b55141d1cdf4941855
Felix Albrecht <mail@felixalbrecht.de>
2013-11-18 21:29:41 +0100

    [playground.algorithms] added __init__.py

---
+++ src/pymor/playground/algorithms/__init__.py
@@ -0,0 +1,5 @@
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright Holders: Felix Albrecht, Rene Milk, Stephan Rave
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+from pymor.playground.algorithms.greedy_lrbms import greedy_lrbms
