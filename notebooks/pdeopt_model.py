99ef84a6bc9596d81ae8efc44b6bd2d1f92d8311
Tim Keil <tim.keil@wwu.de>
2020-09-24 18:18:03 +0200

    state after discussion with Stephan

--- notebooks/pdeopt_model.py
+++ notebooks/pdeopt_model.py
@@ -5,20 +5,27 @@ from pymor.operators.constructions import VectorOperator
 
 class LinearPdeoptStationaryModel(StationaryModel):
     
-    def __init__(self, primal_model, name='LinearPdeoptModel'):
-        super().__init__(primal_model.operator, primal_model.rhs, primal_model.output_functional, 
-                primal_model.products, primal_model.error_estimator, primal_model.visualizer, name)
+    def __init__(self, operator, rhs, output_functional=None, products=None, 
+                 error_estimator=None, visualizer=None, name='LinearPdeoptModel'):
+        super().__init__(operator, rhs, output_functional, products, error_estimator, visualizer, name)
         self.__auto_init(locals())
-        assert primal_model.output_functional.linear
-        self.dual_model = primal_model.with_(rhs=self.output_functional.H)
 
-    def primal_sensitivity(self, parameter, index, mu, U=None):
+    @property
+    def dual_model(self):
+        if not hasattr(self, '_dual_model'):
+            assert self.output_functional is not None
+            assert self.output_functional.linear
+            assert 1 # TODO: how to assert that the operator is symmetric
+            self._dual_model = self.with_(rhs=self.output_functional.H)
+        return self._dual_model
+    
+    def solution_sensitivity(self, parameter, index, mu, U=None): 
         if U is None:
             U = self.solve(mu)
-        residual_dmu_lhs = VectorOperator(self.primal_model.operator.d_mu(parameter, index).apply(U, mu=mu))
-        residual_dmu_rhs = self.primal_model.rhs.d_mu(parameter, index)
+        residual_dmu_lhs = VectorOperator(self.operator.d_mu(parameter, index).apply(U, mu=mu))
+        residual_dmu_rhs = self.rhs.d_mu(parameter, index)
         rhs_operator = residual_dmu_rhs-residual_dmu_lhs
-        return self.primal_model.operator.apply_inverse(rhs_operator.as_range_array(mu), mu=mu)
+        return self.operator.apply_inverse(rhs_operator.as_range_array(mu), mu=mu)
 
     def solve_dual(self, mu):
         return self.dual_model.solve(mu)
@@ -30,16 +37,14 @@ class LinearPdeoptStationaryModel(StationaryModel):
         if adjoint_approach:
             if P is None:
                 P = self.solve_dual(mu)
-        for (parameter, size) in self.primal_model.parameters.items():             
+        for (parameter, size) in self.parameters.items():             
             for index in range(size):
-                output_partial_dmu = self.output_functional.d_mu(parameter, index).apply(U, mu=mu).to_numpy()[0][0]
+                output_partial_dmu = self.output_functional.d_mu(parameter, index).apply(U, mu=mu).to_numpy()[0,0] #inefficient call
                 if adjoint_approach:
-                    residual_dmu_lhs = self.primal_model.operator.d_mu(parameter, index).apply2(U, P, mu=mu)             
-                    residual_dmu_rhs = self.primal_model.rhs.d_mu(parameter, index).apply_adjoint(P, mu=mu).to_numpy()[0][0]
-                    gradient.append((output_partial_dmu + residual_dmu_rhs - residual_dmu_lhs)[0][0])
+                    residual_dmu_lhs = self.operator.d_mu(parameter, index).apply2(U, P, mu=mu)             
+                    residual_dmu_rhs = self.rhs.d_mu(parameter, index).apply_adjoint(P, mu=mu).to_numpy()[0,0]
+                    gradient.append((output_partial_dmu + residual_dmu_rhs - residual_dmu_lhs)[0,0])
                 else:
                     primal_sensitivity = self.primal_sensitivity(parameter, index, mu, U=U)
-                    gradient.append(output_partial_dmu + self.output_functional.apply(primal_sensitivity, mu).to_numpy()[0][0])
+                    gradient.append(output_partial_dmu + self.output_functional.apply(primal_sensitivity, mu).to_numpy()[0,0])
         return np.array(gradient)
-
-
--- notebooks/pdeopt_reductor.py
+++ notebooks/pdeopt_reductor.py
@@ -1,5 +1,3 @@
-from pymor.algorithms.gram_schmidt import gram_schmidt
-from pymor.core.exceptions import ExtensionError
 from pymor.models.basic import StationaryModel
 from pymor.reductors.coercive import CoerciveRBReductor
 
@@ -14,35 +12,4 @@ class LinearPdeoptStationaryCoerciveRBReductor(CoerciveRBReductor):
         assert isinstance(fom, LinearPdeoptStationaryModel)
     
     def build_rom(self, projected_operators, error_estimator):
-        rom = StationaryModel(error_estimator=error_estimator, **projected_operators)
-        return LinearPdeoptStationaryModel(rom)
-
-    def extend_primal_and_dual_basis(self, U, P, basis='RB', method='gram_schmidt', copy_U=True):
-        basis_length = len(self.bases[basis])
-
-        extend_primal_and_dual_basis(U, P, self.bases[basis], self.products.get(basis), method=method, copy_U=copy_U)
-
-        self._check_orthonormality(basis, basis_length)
-
-    def extend_basis(self, mu):
-        U = self.fom.solve(mu)
-        P = self.fom.solve_dual(mu)
-        self.extend_primal_and_dual_basis(U, P)
-
-def extend_primal_and_dual_basis(U, P, basis, product=None, method='gram_schmidt', pod_modes=1, copy_U=True):
-    assert method in ('trivial', 'gram_schmidt', 'pod')
-
-    basis_length = len(basis)
-
-    if method == 'trivial':
-        return NotImplemented
-    elif method == 'gram_schmidt':
-        basis.append(U, remove_from_other=(not copy_U))
-        basis.append(P, remove_from_other=(not copy_U))
-        gram_schmidt(basis, offset=basis_length, product=product, copy=False, check=False)
-        print('My basis size is {}'.format(len(basis)))
-    elif method == 'pod':
-        return NotImplemented
-
-    if len(basis) <= basis_length:
-        raise ExtensionError
+        return LinearPdeoptStationaryModel(error_estimator=error_estimator, **projected_operators)
